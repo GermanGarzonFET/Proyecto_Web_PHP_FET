8c15b31f5ff71c2db749b52143b6636d1538be6d
GermanGarzonFET <53948926+GermanGarzonFET@users.noreply.github.com>
2019-08-22 18:38:13 -0500

    Create trabajo.php

---
+++ trabajo.php
@@ -0,0 +1,388 @@
+<?php
+
+
+
+PRINT <<<HERE
+
+ <body bgcolor=”orange”> </body>
+<backgraund="red">
+    <H1 align="center">
+        <font color="red">
+            Trabajo en casa 
+        </font>
+    </H1> <br>
+    <h2>
+        Aqui presento todos los ejercicios dejados en el taller, agregando aspectos de html basicos deacuerdo con mis conosimientos y lo investigado en internet<br>
+    </h2>
+HERE;
+echo "<br><br>-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-<br>";
+
+//punuto 1.1 
+echo "<h3>Punto 1.1</h3><br>";
+echo "Nos dice cual es el numero mayor entre 10 numeros diferentes, generados al azar entre 0 y 100<br>";
+$num1 = rand(0,100);
+$num2 = rand(0,100);
+$num3 = rand(0,100);
+$num4 =rand(0,100);
+$num5=rand(0,100);
+$num6=rand(0,100) ;
+$num7= rand(0,100);
+$num8= rand(0,100);
+$num9= rand(0,100);
+$num10= rand(0,100);
+echo "los numeros son:<br> 1. $num1 <br>2. $num2<br> 3. $num3 <br>4. $num4 <br>5. $num5<br> 6. $num6<br> 7. $num7 <br>8. $num8<br> 9. $num9<br> 10. $num10  <br>";
+if ($num1>$num2) {
+    if ($num1>$num3) {
+      if ($num1>$num4) {
+          if ($num1>$num5) {
+              if ($num1>$num6) {
+                  if ($num1>$num7) {
+                      if ($num1>$num8) {
+                          if ($num1>$num9) {
+                              if ($num1>$num10) {
+                                  echo"el numero $num1 es el mayor<br>";
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }
+}
+if ($num2>$num1) {
+    if ($num2>$num3) {
+        if ($num2>$num4) {
+            if ($num2>$num5) {
+                if ($num2>$num6) {
+                    if ($num2>$num7) {
+                        if ($num2>$num8) {
+                            if ($num2>$num9) {
+                                if ($num2>$num10) {
+                                    echo"el $num2 es el mayor<br>";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+if ($num3>$num1) {
+    if ($num3>$num2) {
+      if ($num3>$num4) {
+          if ($num3>$num5) {
+              if ($num3>$num6) {
+                  if ($num3>$num7) {
+                      if ($num3>$num8) {
+                          if ($num3>$num9) {
+                              if ($num3>$num10) {
+                                  echo"el numero $num3 es el mayor<br>";
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }
+}
+if ($num4>$num1) {
+    if ($num4>$num2) {
+        if ($num4>$num3) {
+            if ($num4>$num5) {
+                if ($num4>$num6) {
+                    if ($num4>$num7) {
+                        if ($num4>$num8) {
+                            if ($num4>$num9) {
+                                if ($num4>$num10) {
+                                    echo"el $num4 es el mayor<br>";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+if ($num5>$num1) {
+    if ($num5>$num2) {
+      if ($num5>$num3) {
+          if ($num5>$num4) {
+              if ($num5>$num6) {
+                  if ($num5>$num7) {
+                      if ($num5>$num8) {
+                          if ($num5>$num9) {
+                              if ($num5>$num10) {
+                                  echo"el numero $num5 es el mayor<br>";
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }
+}
+if ($num6>$num1) {
+    if ($num6>$num2) {
+        if ($num6>$num3) {
+            if ($num6>$num4) {
+                if ($num6>$num5) {
+                    if ($num6>$num7) {
+                        if ($num6>$num8) {
+                            if ($num6>$num9) {
+                                if ($num6>$num10) {
+                                    echo"el $num6 es el mayor<br>";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+if ($num7>$num1) {
+    if ($num7>$num2) {
+      if ($num7>$num3) {
+          if ($num7>$num4) {
+              if ($num7>$num5) {
+                  if ($num7>$num6) {
+                      if ($num7>$num8) {
+                          if ($num7>$num9) {
+                              if ($num7>$num10) {
+                                  echo"el numero $num7 es el mayor<br>";
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }
+}
+if ($num8>$num1) {
+    if ($num8>$num2) {
+        if ($num8>$num3) {
+            if ($num8>$num4) {
+                if ($num8>$num5) {
+                    if ($num8>$num6) {
+                        if ($num8>$num7) {
+                            if ($num8>$num9) {
+                                if ($num8>$num10) {
+                                    echo"el $num8 es el mayor<br>";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+if ($num9>$num1) {
+    if ($num9>$num2) {
+      if ($num9>$num3) {
+          if ($num9>$num4) {
+              if ($num9>$num5) {
+                  if ($num9>$num6) {
+                      if ($num9>$num7) {
+                          if ($num9>$num8) {
+                              if ($num9>$num10) {
+                                  echo"el numero $num9 es el mayor<br>";
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }
+}
+if ($num10>$num1) {
+    if ($num10>$num2) {
+        if ($num10>$num3) {
+            if ($num10>$num4) {
+                if ($num10>$num5) {
+                    if ($num10>$num6) {
+                        if ($num10>$num7) {
+                            if ($num10>$num8) {
+                                if ($num10>$num9) {
+                                    echo"el $num10 es el mayor<br>";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+echo "<br><br>-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-<br>";
+//punto 1.2
+echo "<h3>punto 1.2</h3><br>";
+echo"Nos dice que dia sera en el mes de Septiembre, generador al azar entre 1 y 30<br>";
+$dia= rand(1,30);
+switch ($dia) {
+    case '1':
+        echo "$dia, <p1>es un domingo</p1>";
+        break;
+    case '2':
+        echo "$dia, es un lunes ";
+        break;
+    case '3':
+        echo "$dia, es un martes";
+        break;
+    case '4':
+        echo "$dia, es un miercoles";
+        break;
+    case '5':
+        echo "$dia, es un jueves";
+        break;
+    case '6':
+        echo "$dia, es un viernes";
+        break;
+    case '7':
+        echo"$dia, es un sabado";
+        break;
+    case '8':
+        echo "$dia, es un domingo";
+        break;
+    case '9':
+        echo "$dia, es un lunes ";
+        break;
+    case '10':
+        echo "$dia, es un martes";
+        break;
+    case '11':
+        echo "$dia, es un miercoles";
+        break;
+    case '12':
+        echo "$dia, es un jueves";
+        break;
+    case '13':
+        echo "$dia, es un viernes";
+        break;
+    case '14':
+        echo"$dia, es un sabado";
+        break;
+    case '15':
+        echo "$dia, es un domingo";
+        break;
+    case '16':
+        echo "$dia, es un lunes ";
+        break;
+    case '17':
+        echo "$dia, es un martes";
+        break;
+    case '18':
+        echo "$dia, es un miercoles";
+        break;
+    case '19':
+        echo "$dia, es un jueves";
+        break;
+    case '20':
+        echo "$dia, es un viernes";
+        break;
+    case '21':
+        echo"$dia, es un sabado";
+        break;
+    case '22':
+        echo "$dia, es un domingo";
+        break;
+    case '23':
+        echo "$dia, es un lunes ";
+        break;
+    case '24':
+        echo "$dia, es un martes";
+        break;
+    case '25':
+        echo "$dia, es un miercoles";
+        break;
+    case '26':
+        echo "$dia, es un jueves";
+        break;
+    case '27':
+        echo "$dia, es un viernes";
+        break;
+    case '28':
+        echo"$dia, es un sabado";
+        break;
+    case '29':
+        echo "$dia, es un domingo";
+        break;
+    case '30':
+        echo "$dia, es un lunes ";
+        break;
+        
+    }
+echo "<br><br>-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-<br>";
+//punto 1.3
+echo "<h3>punto 1.3</h3><br>";
+echo "Genera 3 numeros aleatorios y nos dice cual es menor de 50, cual es menor de 20, y cual es negativo<br>";
+$numero_aleatorio1= rand(-100,100);
+$numero_aleatorio2= rand(-100,100);
+$numero_aleatorio3= rand(-100,100);
+
+echo "<br>El numero aleatorio 1 es: $numero_aleatorio1<br> ";
+echo "<br>El numero aleatorio 2 es: $numero_aleatorio2<br> ";
+echo "<br>El numero aleatorio 3 es: $numero_aleatorio3<br><br> ";
+
+if ($numero_aleatorio1<50) {
+    echo "$numero_aleatorio1 es menor de 50<br>";
+   if ($numero_aleatorio1<20) {
+        echo "$numero_aleatorio1 es menor que 20<br>";
+   }
+   if ($numero_aleatorio1<0) {
+        echo "$numero_aleatorio1 es un numero negativo<br>";
+   }
+} else {
+    echo"$numero_aleatorio1 es mayor que 50<br>";
+};
+
+if ($numero_aleatorio2<50) {
+    echo "$numero_aleatorio2 es menor de 50<br>";
+   if ($numero_aleatorio2<20) {
+        echo "$numero_aleatorio2 es menor que 20<br>";
+   }
+   if ($numero_aleatorio2<0) {
+        echo "$numero_aleatorio2 es un numero negativo<br>";
+   }
+} else {
+    echo"$numero_aleatorio2 es mayor que 50<br>";
+};
+
+if ($numero_aleatorio3<50) {
+    echo "$numero_aleatorio3 es menor de 50<br>";
+   if ($numero_aleatorio3<20) {
+        echo "$numero_aleatorio3 es menor que 20<br>";
+   }
+   if ($numero_aleatorio3<0) {
+        echo "$numero_aleatorio3 es un numero negativo<br>";
+   }
+} else {
+    echo"$numero_aleatorio3 es mayor que 50<br>";
+};
+echo "<br><br>-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-<br>";
+//punto 1.4
+echo "<h3>punto 1.4</h3><br>";
+echo "nos dice cual es par e impar, dependiendo de si el ultimo dijito es dividido por dos y no da decimales<br>";
+$numero = rand();
+if ($numero%2==0)
+    {
+        echo "<br>el $numero es par";
+    }else{
+    echo "<br>el $numero es impar";
+    }
+
+echo"<br><br><br><br><br>";
+//por: German Andres Garzon
+
+
+?>
