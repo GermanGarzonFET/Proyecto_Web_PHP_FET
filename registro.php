0d26a11e89c3fbbb98d2dfb0fc200ca75987accc
GermanGarzonFET <german_garzonto@fet.edu.do>
2019-09-12 16:44:06 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,4 +1,5 @@
 
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -32,7 +33,7 @@
       <div class="collapse navbar-collapse col-3">
         <ul class="navbar-nav">
           <li class="nav-item ">
-            <a class="nav-link" href="#">Inicio </a>
+            <a class="nav-link" href="inicio.php">Inicio </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link" href="#">Pedidos</a>
@@ -41,62 +42,49 @@
             <a class="nav-link" href="#">Acerca de</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link " href="#">Ingresar<span class="sr-only">(current)</span></a>
+            <a class="nav-link " href="login.php">Ingresar<span class="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
     </nav>
 </div>
-<div class="col-12 col-md-4 col-lg">
-  <h4>Completa los campos</h4>
-</div>
+
+
+
     <div class="row">
         <div class="col-12 col-md-4 col-lg"></div>
         <div class="col-12 col-md-4 col-lg">
-         <form action="terminos_condisiones.php" method="post">
-            <div class="form-group row">
-            <div class="container">
-                <img src="img/registro.png" alt=""> 
-              </div>
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Usuario:</label>
-                <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputEmail3" >
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Contraseña:</label>
-                <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword3" >
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-12">
-                <button type="submit" class="btn btn-outline-primary">ver terminos y condiciones</button>
-                </div>
-            </div> 
-          </form >
-        <form action="inicio.php" method="post">
-            <div class="form-group row">
-                <div class="col-sm-6">Acepta los terminos?</div>
-                <div class="col-sm-6">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck1">
-                    <label class="form-check-label" for="gridCheck1">
-                    Acepto los terminos
-                    </label>
-                </div>
-                </div>
-            </div>
+          
+        <form  method="POST" name="form-work" action="base.php ">
 
-            <div class="form-group row">
-                <div class="col-sm-10">
-                <button type="submit" class="btn btn-primary">Registrarme</button>
-                </div>
-            </div>
-         </form>
+        <div class="col-12 col-md-4 col-lg">
+          <h4>Completa los campos para registrarte</h4>
+        </div>
+              <div class="container">
+                  <img src="img/registro.png" alt=""> 
+              </div>
+
+                    <fieldset>
+                      <div class="form-group">
+                            <label class="control-label" for="nome">Nombre</label>
+                            <input name="usuario" class="form-control" placeholder="Tu usuario" type="text">
+                      </div>
+                      <div class="form-group">
+                            <label class="control-label" for="surname">Contraseña</label>
+                            <input name="contrasena" class="form-control" placeholder="Tu contraseña" type="password">
+                        </div>
+                        <div class="form-group">
+                          <div class="col-md-12">
+                            <button type="submit" class="btn btn-outline-primary btn-block">registrarme</button>
+                          </div>
+                        </div>  
+
+
+                    </fieldset> 
+                  </form>
+
         </div>
         <div class="col-12 col-md-4 col-lg"></div>
-
     </div>
     
 <footer class="footer text-center">
@@ -119,3 +107,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
+
+
